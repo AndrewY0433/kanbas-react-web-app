ee9bafd15bfac72f34e51a8fe9710314615327ac
Andrew Yang <yang.and@northeastern.edu>
2023-11-20 21:56:04 -0500

    change

--- src/Labs/a5/WorkingWithArrays.js
+++ src/Labs/a5/WorkingWithArrays.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 function WorkingWithArrays() {
   
     const API = "http://localhost:4000/a5/todos";
@@ -9,6 +10,14 @@ function WorkingWithArrays() {
       due: "2021-09-09",
       completed: false,
     });
+    const [todos, setTodos] = useState([]);
+    const fetchTodos = async () => {
+      const response = await axios.get(API);
+      setTodos(response.data);
+    };
+    useEffect(() => {
+      fetchTodos();
+    }, []);  
       return (
       <div>
         <h3>Working with Arrays</h3>
@@ -26,48 +35,14 @@ function WorkingWithArrays() {
         className="form-control mb-2"
         type="text"
       />
-      <h3>Updating an Item in an Array</h3>
-      <a
-        href={`${API}/${todo.id}/title/${todo.title}`}
-        className="btn btn-primary me-2" >
-        Update Title to {todo.title}
-      </a>
-        <h4>Retrieving Arrays</h4>
-        <a href={API} className="btn btn-primary me-2">
-          Get Todos
-        </a>
-        <h4>Retrieving an Item from an Array by ID</h4>
-      <input
-        className="form-control"
-        value={todo.id}
-        onChange={(e) => setTodo({ ...todo,
-          id: e.target.value })}/>
-      <a href={`${API}/${todo.id}`}
-         className="btn btn-primary me-2">
-        Get Todo by ID
-      </a>
-      <h3>Filtering Array Items</h3>
-      <a href={`${API}?completed=true`}
-        className="btn btn-primary me-2" >
-        Get Completed Todos
-      </a>
-      <h4>Creating new Items in an Array</h4>
-      <a href={`${API}/create`}
-        className="btn btn-primary me-2">
-        Create Todo
-      </a>
-      <input
-        value={todo.id}
-        onChange={(e) => setTodo({
-          ...todo, id: e.target.value })}
-        className="form-control mb-2"
-        type="number"
-      />
-      <h3>Deleting from an Array</h3>
-      <a href={`${API}/${todo.id}/delete`}
-         className="btn btn-primary me-2">
-        Delete Todo with ID = {todo.id}
-      </a>
+      <ul className="list-group">
+        {todos.map((todo) => (
+          <li key={todo.id}
+              className="list-group-item">
+            {todo.title}
+          </li>
+        ))}
+      </ul>
       </div>
     );
   }
