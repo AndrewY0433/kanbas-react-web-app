f6f43a6716439a7e13302bdf30bdb36233e6b756
Andrew Yang <yang.and@northeastern.edu>
2023-11-21 16:59:43 -0500

    changes

--- src/Kanbas/Courses/index.js
+++ src/Kanbas/Courses/index.js
@@ -1,3 +1,5 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 import { Navigate, Route, Routes, useParams } from "react-router-dom";
 import CourseNavigation from "./CourseNavigation";
 import Modules from "./Modules";
@@ -7,9 +9,20 @@ import AssignmentEditor from "./Assignments/AssignmentEditor";
 import Grades from "./Grades"
 
 
-function Courses({ courses }) {
+function Courses() {
   const { courseId } = useParams();
-  const course = courses.find((course) => course._id === courseId);
+  const URL = "http://localhost:4000/api/courses";
+  const [course, setCourse] = useState({});
+  const findCourseById = async (courseId) => {
+    const response = await axios.get(
+      `${URL}/${courseId}`
+    );
+    setCourse(response.data);
+  };
+
+  useEffect(() => {
+    findCourseById(courseId);
+  }, [courseId]);
   return (
     <div>
       <h1>Course {course.name}</h1>
